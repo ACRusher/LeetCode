62e0503ea088cb2d8ab09d90e5f68118ca87debc
xiliang.zxl <job_zhou2214@163.com>
2016-04-24 11:09:53 +0800

    submit code

---
+++ src/main/java/org/ACRusher/leetcode/OJ332.java
@@ -0,0 +1,50 @@
+package org.ACRusher.leetcode;
+
+import java.util.*;
+
+/**
+ * 旅行者线路规划问题
+ * 也是 一个字符串排序问题
+ *
+ * @author xiliang.zxl
+ * @date 2016-04-24 上午9:51
+ */
+public class OJ332 {
+
+
+    public List<String> findItinerary(String[][] tickets) {
+        Map<String,PriorityQueue<String>> ticketMap=new HashMap<String, PriorityQueue<String>>();
+        for(String[] ticket : tickets){
+            ticketMap.computeIfAbsent(ticket[0],k->new PriorityQueue<String>()).add(ticket[1]);
+//            if(!ticketMap.containsKey(ticket[0])) ticketMap.put(ticket[0],new PriorityQueue<String>());
+//            ticketMap.get(ticket[0]).add(ticket[1]);
+        }
+        List<String> itinerary=new ArrayList<String>();
+        find(ticketMap,"JFK",itinerary);
+//        Stack<String> stack=new Stack<String>();
+//        stack.push("JFK");
+//        while (!stack.isEmpty()){
+//            while (ticketMap.containsKey(stack.peek()) && !ticketMap.get(stack.peek()).isEmpty()){
+//                stack.push(ticketMap.get(stack.peek()).poll());
+//            }
+//            itinerary.add(0,stack.pop());
+//        }
+        return itinerary;
+    }
+
+    private void find(Map<String,PriorityQueue<String>>  tickets, String cur,List<String> result){
+        while (tickets.containsKey(cur) && !tickets.get(cur).isEmpty()) {
+            find(tickets, tickets.get(cur).poll(), result);
+        }
+        result.add(0,cur);
+        return;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        String[][] test={{"JFK","SFO"},{"JFK","ATL"},{"SFO","ATL"},{"ATL","JFK"},{"ATL","SFO"}};
+        System.out.println(new OJ332().findItinerary(test));
+    }
+}
